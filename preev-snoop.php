bd388f957c02d3d5713c21a412a533cff1a72b4b
Rama Patel <me@rexsdev.com>
2017-11-29 23:04:31 -0500

    File restructure, created config, cleaned up code.

--- preev-snoop.php
+++ preev-snoop.php
@@ -1,57 +1,42 @@
 <?php
-// SNOOP URL https://api.coindesk.com/v1/bpi/currentprice/USD.json
-
-function getCurlData($url) {
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_URL, $url);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-	curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16");
-	$curlData = curl_exec($curl);
-	curl_close($curl);
-	return $curlData;
-}
-
-function webhook($url, $linkUrl, $linkTitle, $linkDesc, $embedColor, $botName, $botAvatar, $content) {
-  if(isset($embedColor)) {
-    if(strpos($embedColor, "#") > -1) {
-      $c=str_replace("#", "", $embedColor);
-      if (!preg_match("/#([a-fA-F0-9]{3}){1,2}\b/", $c)) {
-        $color = hexdec( strtolower($c) );
-      }
-    }
-  } else {
-    $color = 0;
-  }
-  $sys["content"] = $content;
-  $sys["username"] = $botName;
-  $sys["avatar_url"] = $botAvatar;
-  $e = array("url" => $linkUrl, "title" => $linkTitle, "description" => $linkDesc, "color" => $color);
-  $sys["embeds"] = array(0 => $e);
-  $curl = curl_init($url);
-  curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($sys));
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  curl_exec($curl);
-}
-
-
-$lookup = getCurlData("https://api.coindesk.com/v1/bpi/currentprice/USD.json");
+session_start();
+include 'config.php';
+$lookup = getCurlData($apiURL);
 $obj = json_decode($lookup);
 $rate = $obj->bpi->USD->rate;
 $rate = str_replace(',', '', $rate);
 $rate = round($rate, 2);
 $update = $obj->time->updateduk;
-$url = "https://discordapp.com/api/webhooks/385546436005330945/sWnie_Gdg7nlGodw6nBhtj0SgqjEWhMVUiP1_Q0u9MFJD-JEP6Kj4Po2BRo-zE03ZQQE";
+
+#	DO NOT EDIT
+
+$stmt = $conn->prepare('INSERT INTO rates (`rate`) VALUES (?)');
+$stmt->bind_param("s", $rate);
+$stmt->execute();
+$lastID = $stmt->insert_id;
+$stmt->close();
+
+$lastID = ($lastID - 1);
+
+$stmt3 = $conn->prepare("SELECT rate FROM rates WHERE `id` = ? ORDER BY `id` DESC LIMIT 1");
+$stmt3->bind_param("i", $lastID);
+$stmt3->execute();
+$stmt3->bind_result($row);
+while ($stmt3->fetch()) {
+  $lastRate = $row;
+}
+
 $linkUrl = "http://preev.com/btc/usd";
-$linkTitle = "Current: $". $rate;
 $linkDesc = "Last updated ".$update;
-if($rate > 10000 ){
-$embedColor = "#64dd17";
+
+if($rate > $lastRate ){
+	$linkTitle = ":arrow_up_small: Current: $". $rate;
+	$embedColor = "#64dd17";
 } else {
+	$linkTitle = ":arrow_down_small: Current: $". $rate;
 	$embedColor = "#c62828";
 }
 $botName = "MEGALUL";
 $botAvatar = "https://i.rexsdev.com/xVQzt-DdwRM-yic17.png";
 $content = null;
-webhook($url, $linkUrl, $linkTitle, $linkDesc, $embedColor, $botName, $botAvatar, $content);
+webhook($webhook, $linkUrl, $linkTitle, $linkDesc, $embedColor, $botName, $botAvatar, $content);
